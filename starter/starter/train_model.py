fb3cf5b65e514f658737b232b238c40d9027b30c
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-07 19:04:59 +0800

    setup root path for train model

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -6,8 +6,14 @@ from sklearn.model_selection import train_test_split
 from ml.model import train_model, compute_model_metrics, inference
 from ml.data import process_data
 
+# get root path
+import os
+import sys
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_path)
+
 # Add code to load in the data.
-data = pd.read_csv("../data/census.csv")
+data = pd.read_csv("starter/data/census.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -33,5 +39,12 @@ X_test, y_test, _, _ = process_data(
 # Train and save a model.
 model = train_model(X_train, y_train)
 
-model.save("../starter/model.joblib")
+# Compute and print the model's metrics.
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print(f"Precision: {precision}, Recall: {recall}, F1: {fbeta}")
+
+# save model
+import joblib
+joblib.dump(model, "starter/model/model.joblib")
 
