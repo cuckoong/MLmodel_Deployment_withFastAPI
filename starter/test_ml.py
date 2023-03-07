30e221cb92a15d1daf2d439f062aa58e55656802
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-07 18:50:51 +0800

    initiate model and test

---
+++ starter/test_ml.py
@@ -0,0 +1,15 @@
+import pytest
+
+from starter.ml.model import train_model, compute_model_metrics, inference
+
+
+def test_train_model():
+    assert True
+
+
+def test_compute_model_metrics():
+    assert True
+
+
+def test_inference():
+    assert True
