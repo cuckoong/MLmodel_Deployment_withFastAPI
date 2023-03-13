ca8dfec93ac9194ea3c0d25248c1db9363a8ccce
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-13 16:56:36 +0800

    update the working directory of train_model.py to project root.

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,4 +1,6 @@
 # Script to train machine learning model.
+import os
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -6,43 +8,46 @@ from sklearn.model_selection import train_test_split
 from ml.model import train_model, compute_model_metrics, inference
 from ml.data import process_data
 
-# get root path
+if __name__ == "__main__":
+    # get root path of current project
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+    # set working directory to project root
+    os.chdir(project_root)
+    data = pd.read_csv("starter/data/census.csv")
 
-# Add code to load in the data.
-data = pd.read_csv("/Users/panpan/PycharmProjects/MLOPs/Project3/starter/data/census.csv")
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, test_size=0.20)
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+    # Proces the test data with the process_data function.
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label="salary", training=False,
+        encoder=encoder, lb=lb
+    )
+    # Train and save a model.
+    model = train_model(X_train, y_train)
 
-# Proces the test data with the process_data function.
-X_test, y_test, _, _ = process_data(
-    test, categorical_features=cat_features, label="salary", training=False,
-    encoder=encoder, lb=lb
-)
-# Train and save a model.
-model = train_model(X_train, y_train)
+    # Compute and print the model's metrics.
+    preds, labels = inference(model, X_test, lb)
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    print(f"Precision: {precision}, Recall: {recall}, F1: {fbeta}")
 
-# Compute and print the model's metrics.
-preds = inference(model, X_test)
-precision, recall, fbeta = compute_model_metrics(y_test, preds)
-print(f"Precision: {precision}, Recall: {recall}, F1: {fbeta}")
-
-# save model
-import joblib
-joblib.dump(model, "starter/model/model.joblib")
-joblib.dump(encoder, "starter/model/encoder.joblib")
+    # # save model
+    import joblib
+    joblib.dump(model, "starter/model/model.joblib")
+    joblib.dump(encoder, "starter/model/encoder.joblib")
+    joblib.dump(lb, "starter/model/lb.joblib")
 
