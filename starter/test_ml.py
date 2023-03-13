fd8d34cb6109ce8c85c30a65bf1a6ec7a1633025
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-13 16:51:01 +0800

    correct some path, and add test for model training and inference

--- starter/test_ml.py
+++ starter/test_ml.py
@@ -1,3 +1,4 @@
+import joblib
 import pytest
 import pandas as pd
 import os
@@ -8,7 +9,7 @@ from starter.ml.data import process_data
 
 @pytest.fixture
 def data():
-    df = pd.read_csv("starter/data/census.csv")
+    df = pd.read_csv("data/census.csv")
     return df
 
 
@@ -31,9 +32,23 @@ def categorical_features():
 def label():
     return "salary"
 
+
 @pytest.fixture
-def model_path():
-    return "starter/model/model.joblib"
+def model():
+    model = joblib.load("model/model.joblib")
+    return model
+
+
+@pytest.fixture
+def lb():
+    lb = joblib.load("model/lb.joblib")
+    return lb
+
+
+@pytest.fixture
+def encoder():
+    encoder = joblib.load("model/encoder.joblib")
+    return encoder
 
 
 def test_process_data(data, categorical_features, label):
@@ -44,15 +59,28 @@ def test_process_data(data, categorical_features, label):
     assert lb is not None
 
 
-def test_train_model(model_path):
-    # check if model saved
-    assert os.path.exists(model_path)
+def test_train_model(data, categorical_features, label):
+    X_train, y_train, encoder, lb = process_data(data, categorical_features, label, training=True)
+    model = train_model(X_train, y_train)
+    assert model is not None
 
 
-def test_compute_model_metrics():
-    # load model and test
-    assert True
+def test_model_inference_metrics(model, data, categorical_features, encoder, lb, label):
+    x, y, _, _ = process_data(
+        data, categorical_features=categorical_features, label=label, training=False,
+        encoder=encoder, lb=lb
+    )
+    preds, labels = inference(model, x, lb)
+
+    # check if preds and labels are not None
+    assert preds is not None
+    assert labels is not None
+    assert len(preds) == len(labels) == len(data)
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    # check if metrics are not None
+    assert precision is not None
+    assert recall is not None
+    assert fbeta is not None
 
 
-def test_inference():
-    assert True
