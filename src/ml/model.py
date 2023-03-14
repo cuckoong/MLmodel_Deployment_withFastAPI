bdeb18f1f1161c38256390fdf03c176633590946
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-14 12:09:28 +0800

    add function that outputs the performance of the model on slices of the data

--- src/ml/model.py
+++ src/ml/model.py
@@ -3,6 +3,7 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from src.ml.data import process_data
 
 
 # Optional: implement hyperparameter tuning.
@@ -77,34 +78,41 @@ def inference(model, X, lb=None):
     return preds, labels
 
 
-def slicing_metrics(model, X, y, feature):
+def slicing_metrics(model, df, feature, encoder, lb):
     """
     Compute metrics for a specific feature.
     Parameters
     ----------
     model: sklearn model
         Trained machine learning model.
-    X: np.array
-        Data used for prediction.
-    y: np.array
-        Known labels, binarized.
+    df: pd.DataFrame
+        Data used for prediction, including features and labels.
     feature: str
         Feature to slice on.
+    encoder: sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder.
+    lb: sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer.
 
     Returns
     -------
     res: dict, {category: {precision: float, recall: float, fbeta: float}}
     """
+    # check if feature is in categorical_features
+    X, y, _, _ = process_data(
+        df, label="salary", training=False,
+        encoder=encoder, lb=lb
+    )
+
     preds = model.predict(X)
-    X_featured = X[feature]
+    X_feature = df[feature].copy()
 
     res = {}
 
     # Slice the data.
-    for cat in X_featured.unique():
-        X_slice = X[X[feature] == cat]
-        y_slice = y[X[feature] == cat]
-        preds_slice = preds[X[feature] == cat]
+    for cat in X_feature.unique():
+        y_slice = y[X_feature == cat]
+        preds_slice = preds[X_feature == cat]
         precision, recall, fbeta = compute_model_metrics(y_slice, preds_slice)
         res[cat] = {"precision": precision, "recall": recall, "fbeta": fbeta}
 
--- src/train_model.py
+++ src/train_model.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
-from ml.model import train_model, compute_model_metrics, inference
+from ml.model import train_model, compute_model_metrics, inference, slicing_metrics
 from ml.data import process_data
 
 if __name__ == "__main__":
@@ -37,6 +37,9 @@ if __name__ == "__main__":
     preds, labels = inference(model, X_test, lb)
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
     print(f"Precision: {precision}, Recall: {recall}, F1: {fbeta}")
+
+    # model performance of slicing data
+    slicing_performance = slicing_metrics(model, test, 'sex', encoder, lb)
 
     # # save model
     import joblib
