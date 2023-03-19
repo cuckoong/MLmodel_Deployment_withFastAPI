2f47d1fe248ad273794c85efb789f700f2d5270b
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-19 14:08:16 +0800

    check if model is fitted in test_ml.py

--- test/test_ml.py
+++ test/test_ml.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from src.ml.model import compute_model_metrics, inference
 from src.ml.data import process_data
+from sklearn.exceptions import NotFittedError
 
 
 @pytest.fixture
@@ -43,12 +44,19 @@ def test_process_data(data, label):
     assert lb is not None
 
 
-def test_train_model(model):
+def test_train_model(model, data, label, encoder, lb):
     assert model is not None
     # check if model contains the following steps
     assert "VarianceThreshold" in model.named_steps
     assert "scaler" in model.named_steps
     assert "model" in model.named_steps
+
+    # check if model is fitted
+    x, _, _, _ = process_data(data, label=label, training=False, encoder=encoder, lb=lb)
+    try:
+        model.predict(x)
+    except NotFittedError as e:
+        print(repr(e))
 
 
 def test_model_inference_metrics(model, data, encoder, lb, label):
